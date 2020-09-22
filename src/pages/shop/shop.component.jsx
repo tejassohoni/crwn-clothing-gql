7569c34ec2dd1f8d33d9c87b1e768042fa769620
tejassohoni <tjs1597@gmailcom>
2020-09-22 21:02:04 +0530

    Rework collections-overview and collections to use graphql to fetch data

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import CollectionPageContainer from "../collection/collection.container";
+
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route
+      exact
+      path={`${match.path}`}
+      component={CollectionsOverviewContainer}
+    />
+    <Route
+      path={`${match.path}/:collectionId`}
+      component={CollectionPageContainer}
+    />
+  </div>
+);
+
+export default ShopPage;
